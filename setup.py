1ac92e172c8c7cbee2a5f5fe8f4b45be1dba68d8
frogmane <7685285+xznhj8129@users.noreply.github.com>
2026-08-10 15:31:57 -0400

    Make advanced HiveLink bearers optional extras

--- setup.py
+++ setup.py
@@ -1,38 +1,47 @@
 import sys
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 if sys.version_info < (3, 8):
-    sys.exit('Sorry, Python < 3.8 is not supported.')
+    sys.exit("Sorry, Python < 3.8 is not supported.")
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-    
+
 setup(
     name="hivelink",
-    packages=[package for package in find_packages()],
-    version="0.1",
+    packages=find_packages(),
+    version="0.2",
     license="GPL",
-    description="Hivelink",
+    description="OCCID node-to-node delivery over simple or heterogeneous links",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Frogmane",
     author_email="",
     url="https://github.com/xznhj8129/hivelink",
-    download_url="",
     include_package_data=True,
-    keywords=['udp','meshtastic','manet','mavlink'],
-    install_requires=['geographiclib','mgrs','geojson','crcmod','msgpack','asyncio','pydantic>=2'],
+    keywords=["udp", "mesh", "meshtastic", "occid"],
+    install_requires=[
+        "crcmod",
+        "msgpack",
+        "pydantic>=2",
+    ],
+    extras_require={
+        "mqtt": ["paho-mqtt>=1.6"],
+        "meshtastic": ["frogtastic"],
+        "examples": ["frogcot", "froggeolib", "pymavlink"],
+        "all": [
+            "paho-mqtt>=1.6",
+            "frogtastic",
+            "frogcot",
+            "froggeolib",
+            "pymavlink",
+        ],
+    },
     classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'Intended Audience :: Education',
-          'Intended Audience :: Information Technology',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python',
-          'Framework :: Robot Framework :: Library',
-          'Topic :: Education',
-    ]
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+    ],
 )
