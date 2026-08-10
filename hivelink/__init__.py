07a822b17a050cee6c09e97b823550ac33165147
frogmane <7685285+xznhj8129@users.noreply.github.com>
2026-08-10 15:02:03 -0400

    Expose canonical OCCID payload API

--- hivelink/__init__.py
+++ hivelink/__init__.py
@@ -2,18 +2,14 @@ from . import protocol as _protocol
 
 PROTOCOL_NAME = _protocol.PROTOCOL_NAME
 PROTOCOL_VERSION = _protocol.PROTOCOL_VERSION
-PAYLOAD_MODELS = _protocol.PAYLOAD_MODELS
 message_type = _protocol.message_type
-build_envelope = _protocol.build_envelope
 encode_message = _protocol.encode_message
 decode_message = _protocol.decode_message
 
 __all__ = [
     "PROTOCOL_NAME",
     "PROTOCOL_VERSION",
-    "PAYLOAD_MODELS",
     "message_type",
-    "build_envelope",
     "encode_message",
     "decode_message",
 ]
