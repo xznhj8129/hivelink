f57144759f79b36b85ab654d80235c5906874452
frogmane <7685285+xznhj8129@users.noreply.github.com>
2026-08-15 14:59:52 -0400

    Keep HiveLink package version at 0.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="hivelink",
     packages=find_packages(),
-    version="0.2",
+    version="0.0.1",
     license="GPL",
     description="OCCID node-to-node delivery over simple or heterogeneous links",
     long_description=long_description,
